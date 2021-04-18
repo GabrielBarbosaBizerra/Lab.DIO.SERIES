45f45f92cde87f0c891e8e617d9349c4b731f8ec
Gabriel Barbosa Bizerra <gabrielbarbosabizerra@gmail.com>
2021-04-18 18:50:03 -0300

    Adicionada a interface IRepositorio.cs

---
+++ Interfaces/IRepositorio.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace DIOSeries.Interfaces
+{
+    public interface IRepositorio<T>
+    {
+        //CRUD
+        List<T> Lista();
+        void Inserir(T objeto);
+        void Atualizar(T objeto, int id);
+        void Excluir(int id);
+        void RetornarPorId(int id);
+        int ProximoId();                
+    }
+}
